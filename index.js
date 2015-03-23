997a3a00ee89651f149ac77ac448914fae51cf61
Projjol Banerji <probaner23@gmail.com>
2015-03-23 21:34:04 +0530

    dict modified

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ $(document).ready(function(){
 });
 
 	$("#lotr-form").click(function() {
-		var dict =["Crebain","Kirinki","Mearas","Mûmakil","Wargs","Carc","Gwaihir","Roäc","Thorondor", "od", "roch","Asfaloth","Bill","Brego","Bumpkin","Fatty","Felaróf","Firefoot","Hasufel","Lightfoot","Nahar","Rochallor","Roheryn","Shadowfax","Snowmane","Strider","Stybba","Windfola","Carcharoth","Draugluin","Huan","Shelob","Ungoliant","Dramborleg","Glamdring","Grond","Gurthang","Gúthwinë","Herugrim","Narsil","Orcrist","Ringil","Sting","","Adrahil  ","Adrahil II ","Aegnor","Aerandir","Aghan","Aglahad ","Ailinel","Alatar ","Aldamir","Aldor","Almarian","Almiel","Amandil","Amdír","Amlaith","Amrod","Amroth","Anardil","Anborn","Ancalagon","Andróg","Angbor","Angelimar","Angelimir","Angrod","Anárion","Adûnakhôr","Gimilzôr","Pharazôn","Sakalthôr","Zimrathôn","Ador","Aglas","Agorn","Agorn","Elessar","Agost","Ahad","Ahad","Ahael","Anarth","Anuir","Aphant","Aphor","Assuil","Athorn","Athorn","Aval","Avir","Avorn","Ciryas","Edhel","Geleb","Gonui","Ien","Tamir","Thad","Vedui","Vegil","Veleg","Veleg","Wen","Asgon","Atanamir","Atanatar","Atanatar","Aulë","Azaghâl","Azog "]
+		var dict =["Crebain","Kirinki","Mearas","Mûmakil","Wargs","Carc","Gwaihir","Roäc","Thorondor", "Od", "Roch","Asfaloth","Bill","Brego","Bumpkin","Fatty","Felaróf","Firefoot","Hasufel","Lightfoot","Nahar","Rochallor","Roheryn","Shadowfax","Snowmane","Strider","Stybba","Windfola","Carcharoth","Draugluin","Huan","Shelob","Ungoliant","Dramborleg","Glamdring","Grond","Gurthang","Gúthwinë","Herugrim","Narsil","Orcrist","Ringil","Sting","Adrahil","Aegnor","Aerandir","Aghan","Aglahad ","Ailinel","Alatar ","Aldamir","Aldor","Almarian","Almiel","Amandil","Amdír","Amlaith","Amrod","Amroth","Anardil","Anborn","Ancalagon","Andróg","Angbor","Angelimar","Angrod","Anárion","Adûnakhôr","Gimilzôr","Pharazôn","Sakalthôr","Zimrathôn","Ador","Aglas","Agorn","Elessar","Agost","Ahad","Ahael","Anarth","Anuir","Aphant","Aphor","Assuil","Athorn","Aval","Avir","Avorn","Ciryas","Edhel","Geleb","Gonui","Ien","Tamir","Thad","Vedui","Vegil","Veleg","Wen","Asgon","Atanamir","Atanatar","Aulë","Azaghâl","Azog"]
 
 		var randomNum = function(){
-			return  Math.floor((Math.random() * 110) + 1); 
-		} 
+			return  Math.floor((Math.random() * 100) + 1);
+		}
 
 		var substrNum = function(){
-			return  Math.floor((Math.random() * 3) + 1); 
+			return  Math.floor((Math.random() * 2) + 1);
 		}
 		var text = ""
 		var generate = function(randNum){
@@ -24,7 +24,7 @@ $(document).ready(function(){
 					text +="-";
 				else if (randNum%2 != 0 && randNum-1 != 0)
 					text +="-";
-				randNum -=1 
+				randNum -=1
 			}
 		}
 		generate(4);
